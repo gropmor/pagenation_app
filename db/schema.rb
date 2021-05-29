d8bafb1bd319b92a22f14bfa377c27e045d1a58d
gropmor <rari_atto@yahoo.co.jp>
2021-05-29 16:56:20 +0900

    not null制約の設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_125010) do
+ActiveRecord::Schema.define(version: 2021_05_29_075012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
